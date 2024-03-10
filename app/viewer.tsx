3bec273eee3eb21b118bf43b82a033c688e08581
MswTester <mswtester10417@gmail.com>
2024-03-10 15:25:15 +0900

    image load test

--- app/viewer.tsx
+++ app/viewer.tsx
@@ -13,6 +13,9 @@ export default function Viewer(props: {state: string, setState: Dispatch<SetStat
     const [content, setContent] = useState<any>(null)
     const [selectedData, setSelectedData] = useState<number>(-1)
     const [onFilter, setOnFilter] = useState(false)
+    const [isFetching, setIsFetching] = useState(false)
+    const [title, setTitle] = useState("")
+    const [datatype, setDatatype] = useState("")
 
     useEffect(() => {
         setOnce(true)
@@ -32,6 +35,7 @@ export default function Viewer(props: {state: string, setState: Dispatch<SetStat
     }, [once])
 
     const reload = () => {
+        setIsFetching(true)
         fetch("/api/controller", {
             method: "POST",
             headers: {
@@ -41,12 +45,14 @@ export default function Viewer(props: {state: string, setState: Dispatch<SetStat
                 c: 'list'
             })
         }).then(res => res.json()).then(data => {
+            setIsFetching(false)
             setData(data.data)
         }).catch(err => console.error(err))
     }
 
     return <main className="flex min-h-screen flex-col items-center justify-between pl-40 pr-40 pt-20 pb-20 gap-3">
         <button
+            disabled={isFetching}
             className="absolute left-10 top-8 rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 select-none cursor-pointer"
             onClick={e => props.setState("")}
         >
@@ -59,6 +65,7 @@ export default function Viewer(props: {state: string, setState: Dispatch<SetStat
         </button>
         <nav className="flex flex-row justify-between w-full gap-5">
             <button
+                disabled={isFetching}
                 className="rounded-md bg-neutral-800 dark:bg-gray-50 text-white dark:text-black p-3 text-md font-semibold hover:bg-neutral-700 hover:dark:bg-gray-300 transition-colors"
                 onClick={e => setOnFilter(true)}
             >
@@ -67,12 +74,14 @@ export default function Viewer(props: {state: string, setState: Dispatch<SetStat
             <input className="flex-[8] rounded-md bg p-2 border border-1 border-gray-400 dark:border-neutral-700 bg-gray-200 dark:bg-neutral-900 hover:bg-gray-300 hover:dark:bg-neutral-800 placeholder:text-neutral-600 dark:placeholder:text-gray-300 font-semibold focus:outline-none text-lg transition-colors" type="text" name="" id="" placeholder="Search"
             onChange={e => setSearch(e.target.value)} value={search}/>
             <button
+                disabled={isFetching}
                 className="rounded-md bg-neutral-800 dark:bg-gray-50 text-white dark:text-black p-3 text-md font-semibold hover:bg-neutral-700 hover:dark:bg-gray-300 transition-colors"
                 onClick={reload}
             >
                 Refresh
             </button>
             <button
+                disabled={isFetching}
                 className="rounded-md bg-neutral-800 dark:bg-gray-50 text-white dark:text-black p-3 text-md font-semibold hover:bg-neutral-700 hover:dark:bg-gray-300 transition-colors"
                 onClick={e => setOnWindow(true)}
             >
@@ -89,34 +98,40 @@ export default function Viewer(props: {state: string, setState: Dispatch<SetStat
                     <div className="flex-1"></div>
                 </nav>
                 <div className="w-full h-full flex-1 overflow-y-auto">
-                    {data.map((d, i) => <div key={i}
-                    className="w-full flex flex-row items-center justify-between text-center cursor-pointer hover:dark:bg-neutral-800 transition-colors p-2"
-                    onClick={e => {
-                        if((e.target as Element).nodeName == e.currentTarget.nodeName){
-                            fetch("/api/controller", {
-                                method: "POST",
-                                headers: {
-                                    "Content-Type": "application/json"
-                                },
-                                body: JSON.stringify({
-                                    c: 'read',
-                                    d: {_id: d._id}
-                                })
-                            }).then(res => res.json()).then(data => {
-                                setContent(data.data.content)
-                                setOnContent(true)
-                                setSelectedData(i)
-                            }).catch(err => console.error(err))
-                        }
-                    }}
-                    >
+                    {data.map((d, i) => {
+                        if(!d.name.toLowerCase().includes(search.toLowerCase())) return null
+                        return <div key={i}
+                        className="w-full flex flex-row items-center justify-between text-center cursor-pointer hover:dark:bg-neutral-800 transition-colors p-2"
+                        onClick={e => {
+                            if((e.target as Element).nodeName == e.currentTarget.nodeName){
+                                fetch("/api/controller", {
+                                    method: "POST",
+                                    headers: {
+                                        "Content-Type": "application/json"
+                                    },
+                                    body: JSON.stringify({
+                                        c: 'read',
+                                        d: {_id: d._id}
+                                    })
+                                }).then(res => res.json()).then(data => {
+                                    setContent(data.data.content)
+                                    setTitle(data.data.name)
+                                    setDatatype(data.data.format)
+                                    setOnContent(true)
+                                    setSelectedData(i)
+                                }).catch(err => console.error(err))
+                            }
+                        }}
+                        >
                         <div className="flex-1">{d.name}</div>
                         <div className="flex-1">{d.format}</div>
                         <div className="flex-1">{new Date(d.created).toLocaleDateString()}</div>
                         <div className="flex-1">{new Date(d.modified).toLocaleDateString()}</div>
                         <div className="flex-1 flex flex-row items-center justify-center gap-3">
                             <button className="rounded-md bg-neutral-800 dark:bg-gray-50 text-white dark:text-black p-2 text-md font-semibold hover:bg-neutral-700 hover:dark:bg-gray-300 transition-colors"
+                            disabled={isFetching}
                             onClick={e => {
+                                setIsFetching(true)
                                 fetch("/api/controller", {
                                     method: "POST",
                                     headers: {
@@ -127,13 +142,14 @@ export default function Viewer(props: {state: string, setState: Dispatch<SetStat
                                         d: {_id: d._id}
                                     })
                                 }).then(res => res.json()).then(data => {
+                                    setIsFetching(false)
                                     reload()
                                 }).catch(err => console.error(err))
                             }}>
                                 Remove
                             </button>
                         </div>
-                    </div>)}
+                    </div>})}
                 </div>
             </div>
         }
@@ -151,14 +167,17 @@ export default function Viewer(props: {state: string, setState: Dispatch<SetStat
                 </select>
                 <footer className="w-full flex flex-row items-center justify-between gap-2">
                     <button
+                        disabled={isFetching}
                         className="flex-1 rounded-md bg-neutral-800 dark:bg-gray-50 text-white dark:text-black p-3 text-md font-semibold hover:bg-neutral-700 hover:dark:bg-gray-300 transition-colors placeholder:text-neutral-600 dark:placeholder:text-gray-400"
                         onClick={e => setOnWindow(false)}
                     >
                         Close
                     </button>
                     <button
+                        disabled={isFetching}
                         className="flex-1 rounded-md bg-neutral-800 dark:bg-gray-50 text-white dark:text-black p-3 text-md font-semibold hover:bg-neutral-700 hover:dark:bg-gray-300 transition-colors"
                         onClick={e => {
+                            setIsFetching(true)
                             fetch("/api/controller", {
                                 method: "POST",
                                 headers: {
@@ -169,6 +188,7 @@ export default function Viewer(props: {state: string, setState: Dispatch<SetStat
                                     d: {name, format, created: new Date(), modified: new Date(), content:""}
                                 })
                             }).then(res => res.json()).then(data => {
+                                setIsFetching(false)
                                 setOnWindow(false)
                                 reload()
                             }).catch(err => console.error(err))
@@ -185,12 +205,52 @@ export default function Viewer(props: {state: string, setState: Dispatch<SetStat
                 setSelectedData(-1)
             }}}>
             <div className="bg-white dark:bg-neutral-800 rounded-lg p-10 flex flex-col items-center justify-between gap-5">
-                <h2 className="text-2xl font-semibold">Content</h2>
-                <textarea className="w-full rounded-md bg p-2 border border-1 border-gray-400 dark:border-neutral-700 bg-gray-200 dark:bg-neutral-900 hover:bg-gray-300 hover:dark:bg-neutral-800 placeholder:text-neutral-600 dark:placeholder:text-gray-400 font-semibold focus:outline-none text-lg transition-colors"
-                name="" id="" cols={40} rows={10} value={content}
-                onChange={e => setContent(e.target.value)}/>
+                <input className="text-center text-2xl font-semibold rounded-md p-2 border border-1 border-gray-400 dark:border-neutral-700 bg-gray-200 dark:bg-neutral-900 hover:bg-gray-300 hover:dark:bg-neutral-800 placeholder:text-neutral-600 dark:placeholder:text-gray-400 focus:outline-none transition-colors"
+                type="text" name="" id="" value={title} onChange={e => setTitle(e.target.value)} />
+                <select className="w-full rounded-md bg p-2 border border-1 border-gray-400 dark:border-neutral-700 bg-gray-200 dark:bg-neutral-900 hover:bg-gray-300 hover:dark:bg-neutral-800 placeholder:text-neutral-600 dark:placeholder:text-gray-400 font-semibold focus:outline-none text-lg transition-colors"
+                onChange={e => setDatatype(e.target.value as Format)} value={datatype}>
+                    <option value="text">Text</option>
+                    <option value="img">Image</option>
+                    <option value="json">JSON</option>
+                </select>
+                {
+                    datatype == "img" ? <canvas
+                    width={500} height={300}
+                    className="w-full rounded-md border border-1 border-gray-400 dark:border-neutral-700 bg-gray-200 dark:bg-neutral-900 hover:bg-gray-300 hover:dark:bg-neutral-800 transition-colors"
+                    ref={canvas => {
+                        if(canvas){
+                            let ctx = canvas.getContext("2d")
+                            let img = new Image()
+                            img.src = content
+                            img.onload = () => {
+                                ctx?.drawImage(img, 0, 0, 300, 300)
+                            }
+                        }
+                    }}
+                    onClick={e => {
+                        let file = document.createElement("input")
+                        file.type = "file"
+                        file.accept = "image/*"
+                        file.onchange = e => {
+                            let reader = new FileReader()
+                            reader.onload = e => {
+                                setContent(e.target?.result)
+                            }
+                            reader.readAsDataURL((e.target as HTMLInputElement).files![0])
+                        }
+                        file.click()
+                    }}
+                    ></canvas>:
+                    datatype == "json" ? <textarea className="w-full rounded-md bg p-2 border border-1 border-gray-400 dark:border-neutral-700 bg-gray-200 dark:bg-neutral-900 hover:bg-gray-300 hover:dark:bg-neutral-800 placeholder:text-neutral-600 dark:placeholder:text-gray-400 font-semibold focus:outline-none text-lg transition-colors"
+                    name="" id="" cols={60} rows={15} value={content}
+                    onChange={e => setContent(e.target.value)}/>:
+                    <textarea className="w-full rounded-md bg p-2 border border-1 border-gray-400 dark:border-neutral-700 bg-gray-200 dark:bg-neutral-900 hover:bg-gray-300 hover:dark:bg-neutral-800 placeholder:text-neutral-600 dark:placeholder:text-gray-400 font-semibold focus:outline-none text-lg transition-colors"
+                    name="" id="" cols={60} rows={15} value={content}
+                    onChange={e => setContent(e.target.value)}/>
+                }
                 <footer className="w-full flex flex-row items-center justify-between gap-2">
                     <button
+                        disabled={isFetching}
                         className="flex-1 rounded-md bg-neutral-800 dark:bg-gray-50 text-white dark:text-black p-3 text-md font-semibold hover:bg-neutral-700 hover:dark:bg-gray-300 transition-colors placeholder:text-neutral-600 dark:placeholder:text-gray-400"
                         onClick={e => {
                             setOnContent(false)
@@ -200,9 +260,11 @@ export default function Viewer(props: {state: string, setState: Dispatch<SetStat
                         Cancel
                     </button>
                     <button
+                        disabled={isFetching}
                         className="flex-1 rounded-md bg-neutral-800 dark:bg-gray-50 text-white dark:text-black p-3 text-md font-semibold hover:bg-neutral-700 hover:dark:bg-gray-300 transition-colors placeholder:text-neutral-600 dark:placeholder:text-gray-400"
                         onClick={e => {
                             let seld = data[selectedData]
+                            setIsFetching(true)
                             fetch("/api/controller", {
                                 method: "POST",
                                 headers: {
@@ -211,9 +273,10 @@ export default function Viewer(props: {state: string, setState: Dispatch<SetStat
                                 body: JSON.stringify({
                                     c: 'update',
                                     d: {_id:seld._id},
-                                    m: {content, modified: new Date()}
+                                    m: {content, modified: new Date(), name: title || seld.name}
                                 })
                             }).then(res => res.json()).then(data => {
+                                setIsFetching(false)
                                 setOnContent(false)
                                 reload()
                             }).catch(err => console.error(err))
