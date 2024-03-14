a953409ec63d4e342567287b2aa3a028e06dd60d
MswTester <mswtester10417@gmail.com>
2024-03-14 13:36:35 +0900

    login

--- app/page.tsx
+++ app/page.tsx
@@ -48,7 +48,7 @@ export default function Home() {
   return state == "" ? (
     <main className="flex w-full min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30 select-none cursor-pointer"
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:flex lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30 select-none cursor-pointer"
           onClick={() => setOnlogin(!onlogin)}
         >
           You logged as&nbsp;
@@ -105,8 +105,16 @@ export default function Home() {
         ))}
       </div>
 
-      {onlogin && <div className="absolute w-full h-full left-0 top-0 bg-[#00000055] flex flex-row items-center justify-center">
-
+      {onlogin && <div className="absolute w-full h-full left-0 top-0 bg-[#00000055] flex flex-row items-center justify-center"
+      onMouseDown={e => {
+        if(e.target == e.currentTarget) setOnlogin(false)
+      }}>
+        <div className="flex flex-col gap-4 p-8 bg-white rounded-lg">
+          <p className="text-2xl font-semibold">Login</p>
+          <input type="text" placeholder="Username" className="p-2 border border-gray-300 rounded-lg"/>
+          <input type="password" placeholder="Password" className="p-2 border border-gray-300 rounded-lg"/>
+          <button className="p-2 bg-gradient-to-r from-sky-200 to-blue-200 rounded-lg">Login</button>
+        </div>
       </div>}
     </main>
   ) : state == "role" ? (
